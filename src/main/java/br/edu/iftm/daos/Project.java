9fb73f18828cf48cdb34b1ff22f2ca0da914c76e
Tiago Castro <tiagocastro602@gmail.com>
2024-08-15 11:42:47 +0000

    added `daos`

---
+++ src/main/java/br/edu/iftm/daos/Project.java
@@ -0,0 +1,18 @@
+package br.edu.iftm.daos;
+
+public class Project {
+    public void create() {
+    }
+
+    public void update() {
+    }
+
+    public void delete() {
+    }
+
+    public void list() {
+    }
+
+    public void find() {
+    }
+}
---
+++ src/main/java/br/edu/iftm/daos/Team.java
@@ -0,0 +1,18 @@
+package br.edu.iftm.daos;
+
+public class Team {
+    public void create() {
+    }
+
+    public void update() {
+    }
+
+    public void delete() {
+    }
+
+    public void list() {
+    }
+
+    public void find() {
+    }
+}
