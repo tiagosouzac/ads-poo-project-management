c2328d75f973f1a082f7f5ea6bf29d7509418e66
Tiago Castro <tiagocastro602@gmail.com>
2024-08-25 11:52:58 +0000

    `ListProjectTasks` variations

---
+++ src/main/java/br/edu/iftm/actions/tasks/ListFinishedProjectTasks.java
@@ -0,0 +1,33 @@
+package br.edu.iftm.actions.tasks;
+
+import java.util.List;
+
+import br.edu.iftm.database.daos.TaskDAO;
+import br.edu.iftm.database.models.Project;
+import br.edu.iftm.database.models.Task;
+import br.edu.iftm.database.models.Task.Status;
+
+public class ListFinishedProjectTasks {
+    private final TaskDAO dao = new TaskDAO();
+    private final Project project;
+
+    public ListFinishedProjectTasks(Project project) {
+        this.project = project;
+    }
+
+    public void list() {
+        try {
+            List<Task> tasks = this.dao.list(this.project.getId(), Status.PENDING);
+
+            if (!tasks.isEmpty()) {
+                for (Task task : tasks) {
+                    System.out.println(task);
+                }
+            } else {
+                System.out.println("Nenhum projeto cadastrado ainda!");
+            }
+        } catch (Exception e) {
+            System.out.println("Não foi possível listar as tarefas do projeto. Erro: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/br/edu/iftm/actions/tasks/ListInProgressProjectTasks.java
@@ -0,0 +1,33 @@
+package br.edu.iftm.actions.tasks;
+
+import java.util.List;
+
+import br.edu.iftm.database.daos.TaskDAO;
+import br.edu.iftm.database.models.Project;
+import br.edu.iftm.database.models.Task;
+import br.edu.iftm.database.models.Task.Status;
+
+public class ListInProgressProjectTasks {
+    private final TaskDAO dao = new TaskDAO();
+    private final Project project;
+
+    public ListInProgressProjectTasks(Project project) {
+        this.project = project;
+    }
+
+    public void list() {
+        try {
+            List<Task> tasks = this.dao.list(this.project.getId(), Status.IN_PROGRESS);
+
+            if (!tasks.isEmpty()) {
+                for (Task task : tasks) {
+                    System.out.println(task);
+                }
+            } else {
+                System.out.println("Nenhum projeto cadastrado ainda!");
+            }
+        } catch (Exception e) {
+            System.out.println("Não foi possível listar as tarefas do projeto. Erro: " + e.getMessage());
+        }
+    }
+}
